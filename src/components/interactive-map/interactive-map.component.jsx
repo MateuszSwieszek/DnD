87ef08f8e35c8a2703794be15404bfb57a58df95
Mateusz Swieszek <mateuszswieszek@gmail.com>
2025-03-12 00:21:57 +0100

    another_milestone

--- src/components/interactive-map/interactive-map.component.jsx
+++ src/components/interactive-map/interactive-map.component.jsx
@@ -2,12 +2,57 @@ import { Component } from "react";
 import './interactive-map.styles.css';
 
 class InteractiveMap extends Component {
+    constructor() {
+        super();
+        this.state = {
+            droppedMonsters: {}
+        };
+    }
+
+    onDragOver = (event) => {
+        event.preventDefault();
+    }
+
+    onDrop = (event) => {
+        event.preventDefault();
+        const monsterId = event.dataTransfer.getData('monsterId');
+        const mapRect = event.currentTarget.getBoundingClientRect();
+        const x = event.clientX - mapRect.left;
+        const y = event.clientY - mapRect.top;
+        this.setState(prevState => ({
+            droppedMonsters: {
+                ...prevState.droppedMonsters,
+                [monsterId]: { id: monsterId, x, y }
+            }
+        }));
+    }
+
+    onDragStart = (event, monsterId) => {
+        event.dataTransfer.setData('monsterId', monsterId);
+    }
+
     render() {
+        const { droppedMonsters } = this.state;
         return (
-            <div className='interactive-map'>
-                {/* Interactive Map */}
+            <div 
+                className="interactive-map" 
+                onDrop={this.onDrop} 
+                onDragOver={this.onDragOver}
+            >
+                {Object.values(droppedMonsters).map((monster) => (
+                    <div 
+                        key={monster.id} 
+                        className="monster-icon" 
+                        style={{ left: `${monster.x}px`, top: `${monster.y}px` }}
+                        draggable 
+                        onDragStart={(event) => this.onDragStart(event, monster.id)}
+                    >
+                        {monster.id}
+                    </div>
+                ))}
             </div>
         );
     }
 }
+
 export default InteractiveMap;
